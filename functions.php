617512c4c03fad930cac718d9f7e648cf6beebf6
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-20 13:53:25 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 function startServer(){
   if(!screenOnline()){
+    shell_exec("rm -f /var/craftsrv/servers/x0009/proxy.log*")
     shell_exec("cd /var/craftsrv/servers/x0009/ && screen -dmS sambish20_bungee java -Xmx256M -jar BungeeCord.jar");
   }
 }
