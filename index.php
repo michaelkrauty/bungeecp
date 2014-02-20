e7c204b86546685b3c91b91d0b6062d77c730fca
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-20 12:52:51 -0800

    Update index.php

--- index.php
+++ index.php
@@ -31,9 +31,9 @@
               }
             ?>
             <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-              <button name="start" type="submit" class='btn btn-lg btn-success' <?php if(!serverOnline()){echo "disabled='disabled'";}?>>Start</button>
-              <button name="stop" type="submit" class='btn btn-lg btn-danger' <?php if(serverOnline()){echo "disabled='disabled'";}?>>Stop</button>
-              <button name="restart" type="submit" class='btn btn-lg btn-warning' <?php if(serverOnline()){echo "disabled='disabled'";}?>>Restart</button>
+              <button name="start" type="submit" class='btn btn-lg btn-success' <?php# if(!serverOnline()){echo "disabled='disabled'";}?>>Start</button>
+              <button name="stop" type="submit" class='btn btn-lg btn-danger' <?php# if(serverOnline()){echo "disabled='disabled'";}?>>Stop</button>
+              <button name="restart" type="submit" class='btn btn-lg btn-warning' <?php# if(serverOnline()){echo "disabled='disabled'";}?>>Restart</button>
             </form>
           </div>
         </center>
