ded593d1ee62c1951166503e12f34b2ca07b06a0
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-20 13:43:24 -0800

    Update console.inc.php

--- console.inc.php
+++ console.inc.php
@@ -1,2 +1,2 @@
 <?php $text = file_get_contents("/var/craftsrv/servers/x0009/proxy.log.0"); ?>
-<textarea name="text"><?php echo htmlspecialchars($text); ?></textarea>
+<textarea name="text" class="console"><?php echo htmlspecialchars($text); ?></textarea>
