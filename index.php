064cfb5691fb78dccb2ffa8e091848706397d886
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-20 13:39:09 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
 <html lang='en'>
 <head>
+  <title>BungeeCP</title>
+  <meta name="author" content="Michael Krautkramer">
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="Michael Krautkramer">
-  <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
-  <title>BungeeCP</title>
-  <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="css/index.php.css" rel="stylesheet">
-  <link href="./bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
-  <link href="webroot/css/index.css" rel="stylesheet">
+  <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
 </head>
 <body role='document'> <body role='document'>
   <?php include_once "functions.php"; ?>
