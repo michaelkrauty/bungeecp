d6606e546de811e66b90446bdd547c2948336a43
michaelkrauty <michaelkrauty@users.noreply.github.com>
2014-02-20 12:35:57 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,21 +1,21 @@
 <?php
 
 function startServer(){
-  shell_exec("screen -x sambish20bungee -p 0 -X stuff \"`printf \".unhold\r\"`\";");
+  shell_exec("screen -x testbungee -p 0 -X stuff \"`printf \".unhold\r\"`\";");
 }
 
 function stopServer(){
-  shell_exec("screen -x sambish20bungee -p 0 -X stuff \"`printf \".hold\r\"`\";");
+  shell_exec("screen -x testbungee -p 0 -X stuff \"`printf \".hold\r\"`\";");
 }
 
 function restartServer(){
-  shell_exec("screen -x sambish20bungee -p 0 -X stuff \"`printf \"end\r\"`\";");
+  shell_exec("screen -x testbungee -p 0 -X stuff \"`printf \"end\r\"`\";");
 }
 
 function serverOnline(){
   include_once 'status.class.php';
   $status = new MinecraftServerStatus();
-  $response = $status->getStatus("192.187.118.205", 25565);
+  $response = $status->getStatus("192.187.118.202", 65437);
   if($response){
     return true;
   }else{
